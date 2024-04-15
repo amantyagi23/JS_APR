f0378528424ed191c68d1285f1ae8bc10236f197
Aman <119286791+amantyagi23@users.noreply.github.com>
2024-04-15 12:56:44 +0530

    Add files via upload

---
+++ checkNumber.js
@@ -0,0 +1,37 @@
+
+var a = 9;
+
+// case 
+if(a==0){
+    console.log("Zero");
+}
+else if(a==1){
+    console.log("One");
+}
+else if(a==2){
+    console.log("Two");
+}
+else if(a==3){
+
+}
+else if(a==4){
+
+}
+else if(a==5){
+
+}
+else if(a==6){
+
+}
+else if(a==7){
+
+}
+else if(a==8){
+
+}
+else if(a==9){
+    console.log("Nine");
+}
+else{
+    console.log("Invalid number");
+}
---
+++ fissbuss.js
@@ -0,0 +1,11 @@
+var a = 25;
+
+if(a %3 ==0){
+    console.log("Fizz");
+}
+if(a%5 ==0){
+    console.log("Buzz");
+}
+else{
+    console.log("FizzBuzz");
+}
