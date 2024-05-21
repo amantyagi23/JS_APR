46cd5af977f349544d7c99c67575dd9586d166ef
amantyagi23 <amantyagi4987@gmail.com>
2024-05-21 13:02:31 +0530

    changes

---
+++ Object/index.js
@@ -0,0 +1,21 @@
+var a = "operator"
+// + - = operations
+
+// object 
+
+
+
+// key or value 
+var object = {
+    name :"Aman",
+    age:18,
+    branch : "cse",
+    adult:false,
+    arr :[],
+    add:function(a,b){ return a+b}
+}
+
+
+var arr = [object,object]
+
+console.log(arr);
---
+++ Object/variable.js
@@ -0,0 +1,14 @@
+
+console.log(a);
+// host 
+var a = 10;
+a++
+console.log(a);
+let b = 10;
+a++;
+b++;
+console.log(a,b);
+const c = 11;
+c+1;
+c = "129"
+console.log(c);
